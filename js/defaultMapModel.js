1af9b70de3875b5bb25d426e741ccbf5b03b64a3
SergeyRyzhkov <admin@ryzhkov-sn.ru>
2015-11-14 20:17:09 +0300

    sddsf

---
+++ js/defaultMapModel.js
@@ -0,0 +1,294 @@
+defaultMapModel = {
+
+	"id": "vsm_map",
+	"name": "Скоростные магистрали",
+	"displayName": "Скоростные магистрали",
+	"description": "Скоростные магистрали",
+	"options": {
+		"zoom": 5,
+		"center": [56.13, 30],
+		"zoomControl": false
+	},
+
+	"layers": [{
+			"id": "OpenStreetMap",
+			"name": "OpenStreetMap",
+			"displayName": "OpenStreetMap",
+			"description": "OpenStreetMap",
+			"type": "base",
+			"visible": true,
+			"visibleIndex": 0,
+			"minZoom": 0,
+			"maxZoom": 23,
+			"selectable": false,
+			"queryable": false,
+
+			"layerClass": {
+				"constructor": "MapExpress.Layers.TileServiceLayer",
+				"args": [
+
+				],
+				"options": {
+					"attribution": "OpenStreetMap"
+				}
+			},
+
+			"dataProviderClass": {
+				"constructor": "MapExpress.Service.TileProvider",
+				"args": [
+					"http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				],
+				"options": {}
+			}
+		}, {
+			"id": "2ГИС",
+			"name": "2ГИС",
+			"displayName": "2ГИС",
+			"description": "2ГИС",
+			"type": "base",
+			"visible": false,
+			"visibleIndex": 0,
+			"minZoom": 0,
+			"maxZoom": 23,
+			"selectable": false,
+			"queryable": false,
+
+			"layerClass": {
+				"constructor": "MapExpress.Layers.TileServiceLayer",
+				"args": [],
+				"options": {
+					"attribution": "2ГИС"
+				}
+			},
+
+			"dataProviderClass": {
+				"constructor": "MapExpress.Service.TileProvider",
+				"args": [
+					"http://tile{s}.maps.2gis.com/tiles?x={x}&y={y}&z={z}&v=1"
+				],
+				"options": {
+					"subdomains": "123"
+				}
+			}
+		}, {
+			"id": "Росреестр РФ ГКН",
+			"name": "Росреестр РФ ГКН",
+			"displayName": "Росреестр РФ ГКН",
+			"description": "Росреестр РФ ГКН",
+			"type": "overlay",
+			"visible": false,
+			"visibleIndex": 0,
+			"minZoom": 0,
+			"maxZoom": 23,
+			"selectable": false,
+			"queryable": true,
+
+			"layerClass": {
+				"constructor": "MapExpress.Layers.TileServiceLayer",
+				"args": [],
+				"options": {
+					"attribution": "Росреестр РФ"
+				}
+			},
+
+			"dataProviderClass": {
+				"constructor": "MapExpress.Service.MapServiceAgsProvider",
+				"args": [
+					"http://maps.rosreestr.ru/arcgis/rest/services/Cadastre/Cadastre/MapServer"
+				],
+				"options": {
+					"identifyLayersId": "0,1",
+					"identifyUrl": "http://maps.rosreestr.ru/arcgis/rest/services/Cadastre/CadastreSelected/MapServer"
+				}
+			}
+		}
+		, {
+			"id": "ЗОУИТ ГКН",
+			"name": "ЗОУИТ ГКН",
+			"displayName": "ЗОУИТ ГКН",
+			"description": "ЗОУИТ ГКН",
+			"type": "overlay",
+			"visible": false,
+			"visibleIndex": 0,
+			"minZoom": 0,
+			"maxZoom": 23,
+			"selectable": false,
+			"queryable": true,
+
+			"layerClass": {
+				"constructor": "MapExpress.Layers.TileServiceLayer",
+				"args": [],
+				"options": {
+					"attribution": "Росреестр РФ"
+				}
+			},
+
+			"dataProviderClass": {
+				"constructor": "MapExpress.Service.MapServiceAgsProvider",
+				"args": [
+					"http://maps.rosreestr.ru/arcgis/rest/services/Cadastre/ZOUIT/MapServer"
+				],
+				"options": {
+					"identifyLayersId": "0",
+					"identifyUrl": "http://maps.rosreestr.ru/arcgis/rest/services/Cadastre/ZOUIT/MapServer"
+				}
+			}
+		}, 
+		{
+			"id": "Границы ГКН",
+			"name": "Границы ГКН",
+			"displayName": "Границы ГКН",
+			"description": "Границы ГКН",
+			"type": "overlay",
+			"visible": false,
+			"visibleIndex": 0,
+			"minZoom": 0,
+			"maxZoom": 23,
+			"selectable": false,
+			"queryable": true,
+
+			"layerClass": {
+				"constructor": "MapExpress.Layers.TileServiceLayer",
+				"args": [],
+				"options": {
+					"attribution": "Росреестр РФ"
+				}
+			},
+
+			"dataProviderClass": {
+				"constructor": "MapExpress.Service.MapServiceAgsProvider",
+				"args": [
+					"http://maps.rosreestr.ru/arcgis/rest/services/Cadastre/BordersGKNSelected/MapServer"
+				],
+				"options": {
+					"identifyLayersId": "7,8,9,10.11,12,13,14,15,16,17,18",
+					"identifyUrl": "http://maps.rosreestr.ru/arcgis/rest/services/Cadastre/BordersGKNSelected/MapServer"
+				}
+			}
+		}, 
+
+		{
+			"id": "Участки ВСМ",
+			"name": "Участки ВСМ",
+			"displayName": "Участки ВСМ",
+			"description": "Участки ВСМ",
+			"type": "overlay",
+			"visible": true,
+			"visibleIndex": 0,
+			"minZoom": 0,
+			"maxZoom": 23,
+			"selectable": false,
+			"queryable": true,
+
+			"layerClass": {
+				"constructor": "MapExpress.Layers.GeoJSONServiceLayer",
+				"args": [],
+				"options": {
+					"style": {
+						"color": "blue",
+						"weight": 2,
+						"opacity": 0.5,
+						"fillOpacity": 0.1
+					}
+				}
+			},
+
+			"dataProviderClass": {
+				"constructor": "MapExpress.Service.GeoJSONProvider",
+				"args": [
+					"./data/geojson-sample.json"
+				]
+			}
+		}, {
+			"id": "Полоса отвода",
+			"name": "Полоса отвода",
+			"displayName": "Полоса отвода",
+			"description": "Полоса отвода",
+			"type": "overlay",
+			"visible": true,
+			"visibleIndex": 0,
+			"minZoom": 0,
+			"maxZoom": 23,
+			"selectable": false,
+			"queryable": true,
+
+			"layerClass": {
+				"constructor": "MapExpress.Layers.GeoJSONServiceLayer",
+				"args": [],
+				"options": {
+					"style": {
+						"color": "green",
+						"weight": 2,
+						"opacity": 0.5,
+						"fillOpacity": 0.1
+					}
+				}
+			},
+
+			"dataProviderClass": {
+				"constructor": "MapExpress.Service.GeoJSONProvider",
+				"args": [
+					"./data/boundary_wgs.json"
+				]
+			}
+		}, {
+			"id": "Google",
+			"name": "Google",
+			"displayName": "Google",
+			"description": "Google",
+			"type": "base",
+			"visible": false,
+			"visibleIndex": 0,
+			"minZoom": 0,
+			"maxZoom": 23,
+			"selectable": false,
+			"queryable": true,
+
+			"layerClass": {
+				"constructor": "MapExpress.Layers.TileServiceLayer",
+				"args": [],
+				"options": {
+					"attribution": "Google"
+				}
+			},
+
+			"dataProviderClass": {
+				"constructor": "MapExpress.Service.TileProvider",
+				"args": [
+					"https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+				]
+			}
+		}, {
+			"id": "Bing",
+			"name": "Bing",
+			"displayName": "Bing",
+			"description": "Bing",
+			"type": "base",
+			"visible": false,
+			"visibleIndex": 0,
+			"minZoom": 0,
+			"maxZoom": 23,
+			"selectable": false,
+			"queryable": true,
+
+			"layerClass": {
+				"constructor": "MapExpress.Layers.TileServiceLayer",
+				"args": [],
+				"options": {
+					"attribution": "Bing"
+				}
+			},
+
+			"dataProviderClass": {
+				"constructor": "MapExpress.Service.TileProvider",
+				"args": [
+					"http://ecn.t3.tiles.virtualearth.net/tiles/a{q}.jpeg?g=0&dir=dir_n"
+				],
+				"options": {
+					"useQuadkey": true
+				}
+			}
+		}
+
+	]
+}
---
+++ js/initializeMap.js
@@ -0,0 +1,146 @@
+function InitializeMap(mapDiv) {
+	var map = L.map(mapDiv, {
+		zoomControl: false,
+		editable: true
+	}).setView([56.13, 47.25], 9);
+
+	var mapManager = new MapExpress.Mapping.MapManager(map);
+	mapManager.renderMap(defaultMapModel);
+
+	L.control.layers(mapManager._getBaseObj(), mapManager._getOverlayObj()).addTo(map);
+
+	var toolbar = new MapExpress.Tools.MapToolbar(mapManager);
+	var layersCommand = MapExpress.Service.showLayerControlMapCommand(mapManager);
+	var idCommand = MapExpress.Service.identifyMapCommand(mapManager);
+
+	var createParcelCommand = new MapExpress.Tools.CreateParcelCommand(mapManager);
+	var createLineCommand = new MapExpress.Tools.CreateLineCommand(mapManager);
+	var createPointCommand = new MapExpress.Tools.CreatePointCommand(mapManager);
+	
+
+	toolbar.addCommand(layersCommand);
+	toolbar.addCommand(idCommand);
+	toolbar.addCommand(createParcelCommand);
+	toolbar.addCommand(createLineCommand);
+	toolbar.addCommand(createPointCommand);
+	
+
+	map.addControl(toolbar);
+	
+
+	L.control.zoom({
+		position: 'topleft'
+	}).addTo(map);
+
+	L.control.scale().addTo(map, {
+		imperial: true
+	});
+
+	return map;
+};
+
+MapExpress.Tools.CreateParcelCommand = MapExpress.Tools.BaseMapCommand.extend({
+	options: {
+		buttonClassName: 'btn btn-default btn-sm text-center'
+	},
+
+	initialize: function(mapManager, options) {
+		MapExpress.Tools.BaseMapCommand.prototype.initialize.call(this, mapManager, options);
+		L.setOptions(this, options);
+		this._active = false;
+		this.editLayer = this._mapManager.getLayerById("Участки ВСМ");
+	},
+
+	createContent: function(toolBarContainer) {
+		var button = L.DomUtil.create('button', this.options.buttonClassName, toolBarContainer);
+		var li = L.DomUtil.create('i', 'fa fa-object-group fa-lg fa-fw', button);
+		button.setAttribute('data-toggle', 'tooltip');
+		button.setAttribute('data-placement', 'bottom');
+		button.setAttribute('title', 'Создать ЗУ');
+		return button;
+	},
+
+	activate: function() {
+		this._mapManager._map.editTools.startPolygon();
+		if (!this._active) {
+			//this._mapManager._map.on('editable:drawing:end', this._featureAdded, this)
+			//this._mapManager._map.on('editable:editing', function(e) {
+			//	e.layer.setStyle({
+			//		color: 'DarkRed'
+			//	});
+			//});
+		}
+		this._active = true;
+	},
+
+	_featureAdded: function(e) {
+		e.layer.feature = {
+			"properties": {
+				"КН": "Кадастровый номер ЗУ",
+				"Примечание": "Текст",
+				"Идентификатор": e.layer._leaflet_id
+			}
+		};
+		this.editLayer.addLayer(e.layer);
+		console.log(e.layer);
+	}
+
+});
+
+MapExpress.Tools.CreateLineCommand = MapExpress.Tools.BaseMapCommand.extend({
+	options: {
+		buttonClassName: 'btn btn-default btn-sm text-center'
+	},
+
+	initialize: function(mapManager, options) {
+		MapExpress.Tools.BaseMapCommand.prototype.initialize.call(this, mapManager, options);
+		L.setOptions(this, options);
+		this._active = false;
+	},
+
+	createContent: function(toolBarContainer) {
+		var button = L.DomUtil.create('button', this.options.buttonClassName, toolBarContainer);
+		var li = L.DomUtil.create('i', 'fa fa-arrows-h fa-lg fa-fw', button);
+		button.setAttribute('data-toggle', 'tooltip');
+		button.setAttribute('data-placement', 'bottom');
+		button.setAttribute('title', 'Создать линию');
+		return button;
+	},
+
+	activate: function() {
+		this._mapManager._map.editTools.startPolyline();
+		if (!this._active) {
+			//this._mapManager._map.on('editable:editing', function(e) {
+			//	e.layer.setStyle({
+			//		color: 'DarkBlue'
+			//	});
+			//});
+		}
+		this._active = true;
+	}
+});
+
+
+MapExpress.Tools.CreatePointCommand = MapExpress.Tools.BaseMapCommand.extend({
+	options: {
+		buttonClassName: 'btn btn-default btn-sm text-center'
+	},
+
+	initialize: function(mapManager, options) {
+		MapExpress.Tools.BaseMapCommand.prototype.initialize.call(this, mapManager, options);
+		L.setOptions(this, options);
+	},
+
+	createContent: function(toolBarContainer) {
+		var button = L.DomUtil.create('button', this.options.buttonClassName, toolBarContainer);
+		var li = L.DomUtil.create('i', 'fa fa-map-pin fa-lg fa-fw', button);
+		button.setAttribute('data-toggle', 'tooltip');
+		button.setAttribute('data-placement', 'bottom');
+		button.setAttribute('title', 'Создать точку');
+		return button;
+	},
+
+	activate: function() {
+		this._mapManager._map.editTools.startMarker();
+	}
+});
